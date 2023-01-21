27c27968efefdefc49476dbe5e2d1f57db645f9d
Ankit Kayastha <57693466+ankit-11@users.noreply.github.com>
2023-01-21 12:29:20 +0545

    Update frameSample.py

--- FrameSample/frameSample.py
+++ FrameSample/frameSample.py
@@ -2,7 +2,8 @@ import cv2
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Frame Sampling for 1 frame per second')
+parser = argparse.ArgumentParser(
+    description='Frame Sampling for 1 frame per second')
 parser.add_argument('-v', type=str)
 parser.add_argument('-d', type=str)
 args = parser.parse_args()
@@ -10,19 +11,26 @@ args = parser.parse_args()
 video = cv2.VideoCapture(args.v)
 
 os.chdir(args.d)
-currentframe=1
+currentframe = 1
 fps = int(video.get(cv2.CAP_PROP_FPS))
 print('FPS Of Video = ' + str(fps))
 
-while(video.isOpened()):
-    ret,frame = video.read() 
+# i = 1 for first image
+i = 1  # Change value of i as necessary 
+
+while (video.isOpened()):
+    ret, frame = video.read()
     if ret:
-        if(currentframe%fps==0):
-            name = str(int(currentframe/fps)) + '.jpg'
-            print('Picture saved :::'+ str(int(currentframe/fps)) + '.jpg')
-            cv2.imwrite(name, frame) 
+        if (currentframe % fps == 0):
+            # For unique naming, 
+            # Use alphabest for user: A for Anish, B for Ankit, P for Pragya
+            # Use alphabets for time: M for morning, D for day, E for Evening
+            name = 'user' + 'time' + str(i) + '.jpg'
+            print('Picture saved :::' + 'user' + 'time' + str(i) + '.jpg')
+            cv2.imwrite(name, frame)
+            i = i+1
         currentframe += 1
-    else: 
+    else:
         break
-video.release() 
+video.release()
 cv2.destroyAllWindows()
